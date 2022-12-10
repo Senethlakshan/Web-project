967c2b291b5d221543ed08490064121303961aa7
Isitha <72751254+Isitha@users.noreply.github.com>
2022-12-10 13:13:17 +0530

    sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,37 +1,54 @@
 import React, { useState } from "react";
-import { HiOutlineMoon } from "react-icons/hi";
-import { MdKeyboardArrowRight } from "react-icons/md";
+import { HiShoppingCart } from "react-icons/hi";
+import { MdKeyboardArrowRight, MdCategory } from "react-icons/md";
 
 const Sidebar = () => {
   const [open, setOpen] = useState(false);
 
   const productItems = [
-    { title: "Mobile Phones", icons: <HiOutlineMoon /> },
-    { title: "Mobile Phones", icons: <HiOutlineMoon /> },
-    { title: "Mobile Phones", icons: <HiOutlineMoon /> },
-    { title: "Mobile Phones", icons: <HiOutlineMoon /> },
-    { title: "Mobile Phones", icons: <HiOutlineMoon /> },
-    { title: "Mobile Phones", icons: <HiOutlineMoon /> },
-    { title: "Mobile Phones", icons: <HiOutlineMoon /> },
+    { title: "Categories", icon: <MdCategory />, isTitle: true },
+    { title: "Mobile Phones", icon: <HiShoppingCart /> },
+    { title: "Mobile Phones", icon: <HiShoppingCart /> },
+    { title: "Mobile Phones", icon: <HiShoppingCart /> },
+    { title: "Mobile Phones", icon: <HiShoppingCart /> },
+    { title: "Mobile Phones", icon: <HiShoppingCart /> },
+    { title: "Mobile Phones", icon: <HiShoppingCart /> },
+    { title: "Mobile Phones", icon: <HiShoppingCart /> },
   ];
 
   return (
     <div
       className={`relative ${
         open ? "w-52" : "w-16"
-      } bg-green-600 py-2 duration-300`}
+      } bg-green-600 py-2 transition-all duration-300`}
     >
       <div
-        className="absolute -right-4 top-10 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full border-4 border-green-600 bg-white text-xl text-green-600"
+        className="absolute -right-4 top-14 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full border-4 border-green-600 bg-white text-xl text-green-600"
         onClick={() => {
           setOpen(!open);
         }}
       >
         <MdKeyboardArrowRight />
       </div>
-      <div className="text-center uppercase text-white">
-        <h1>Categories</h1>
-      </div>
+      <ul className="space-y-4 p-2">
+        {productItems.map((item, index) => (
+          <li
+            key={index}
+            className={`${
+              item.isTitle && "pointer-events-none mb-10"
+            } flex cursor-pointer items-center space-x-4 rounded-lg px-3 py-2 text-green-100 hover:bg-green-200 hover:text-green-600`}
+          >
+            <div className="pointer-events-none text-2xl">{item.icon}</div>
+            <a
+              className={`whitespace-nowrap ${
+                !open && "scale-0"
+              } pointer-events-none origin-left transform transition-transform duration-300`}
+            >
+              {item.title}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
