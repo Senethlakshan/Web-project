c92008f501647749bdfd3e0e56d0a1906c21b8fb
Isitha <72751254+Isitha@users.noreply.github.com>
2022-12-10 13:47:14 +0530

    change sidebar text

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -35,14 +35,14 @@ const Sidebar = () => {
           <li
             key={index}
             className={`${
-              item.isTitle && "pointer-events-none mb-10"
+              item.isTitle && "pointer-events-none mb-10 font-bold uppercase"
             } flex cursor-pointer items-center space-x-4 rounded-lg px-3 py-2 text-green-100 hover:bg-green-200 hover:text-green-600`}
           >
             <div className="pointer-events-none text-2xl">{item.icon}</div>
             <a
               className={`whitespace-nowrap ${
                 !open && "scale-0"
-              } pointer-events-none origin-left transform transition-transform duration-300`}
+              } pointer-events-none origin-left transform font-medium transition-transform duration-300`}
             >
               {item.title}
             </a>
