1c1b825fa87f84f671407b081ca652e1598c4bb2
Isitha <72751254+Isitha@users.noreply.github.com>
2022-12-10 12:00:40 +0530

    Navigation bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,40 @@ const Header = () => {
     { title: "Contact", link: "#" },
   ];
 
-  return <div className=""></div>;
+  return (
+    <div className="relative h-[8rem] bg-green-800 text-white">
+      <div className="relative flex items-center justify-between px-10 py-4 text-[1.8rem]">
+        <div>Logo</div>
+        <ul className="absolute top-1/2 left-1/2 flex -translate-y-1/2 -translate-x-1/2 transform items-center space-x-10 font-semibold uppercase">
+          {menu.map((item, index) => (
+            <li key={index}>
+              <a href={item.link}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex items-center space-x-5">
+          <div className="flex cursor-pointer items-center justify-center rounded-lg border-2 border-white py-1 px-3">
+            <a>Sign in</a>
+          </div>
+          <div className="flex cursor-pointer items-center justify-center rounded-lg bg-white py-1 px-3 text-green-800">
+            <a>Sign up</a>
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-[6rem] left-1/2 flex h-[5rem] w-[50rem] -translate-x-1/2 transform rounded-full bg-white shadow-lg">
+        <input
+          type="search"
+          name="search"
+          id="search"
+          placeholder="Search Product"
+          className="ml-[2.5rem] w-full border-none bg-transparent text-gray-500 outline-none"
+        />
+        <div className="flex h-full w-[14rem] cursor-pointer items-center justify-center rounded-full bg-green-600 text-[1.6rem] font-medium">
+          <a>Search</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
